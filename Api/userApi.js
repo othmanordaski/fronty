099e23226f2ca2624c86ef72e09a9b5cdf9f9b6d
othmane errouissat <rouissatothman@gmail.com>
2024-05-12 19:13:03 +0100

    Implement axios API calls for user registration, email verification, and password reset

--- Api/userApi.js
+++ Api/userApi.js
@@ -11,3 +11,15 @@ const api = axios.create({
 export function registerClient(body) {
     return api.post('/client/register', body)
 }
+export function registerUser(body) {
+    return api.post('/client/register/user', body)
+}
+export function verifyEmail(id,token) {
+    return api.get(`/client/verify/${id}/${token}`)
+}
+export function forgetPassword (email) {
+    return api.post('/client/forgot-password',{email : email})
+}
+export function resetPassword (token, body) {
+    return api.post(`/client/reset-password/${token}`, body)
+}
