3d858058b4e7cf496477fd57b23e1cee3946258e
othmane errouissat <rouissatothman@gmail.com>
2024-06-01 00:37:22 +0100

    add toast message handler

---
+++ src/pages/Dashboards/admin/ToastMessageHandler.jsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { useEffect } from 'react';
+import { ToastAction } from "@/components/ui/toast"
+import { useToast } from "@/components/ui/use-toast"
+
+export function ToastDestructive({ title, description }) {
+  const { toast } = useToast()
+
+  useEffect(() => {
+    toast({
+      variant: "destructive",
+      title: title || "Uh oh! Something went wrong.",
+      description: description || "There was a problem with your request.",
+      action: <ToastAction altText="Try again">Try again</ToastAction>,
+    })
+  }, []);
+
+  return null;
+}
+
+export function ToastWithTitle({ title, description }) {
+  const { toast } = useToast()
+
+  useEffect(() => {
+    toast({
+      title: title || "Uh oh! Something went wrong.",
+      description: description || "There was a problem with your request.",
+    })
+  }, []);
+
+  return null;
+}
+
+export function ToastSimple({ description }) {
+  const { toast } = useToast()
+
+  useEffect(() => {
+    toast({
+      className: "bg-green-500 h-[24px] text-white",
+      description: description || "The user has been deleted.",
+    })
+  }, []);
+
+  return null;
+}
