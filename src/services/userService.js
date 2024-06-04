42b2ec99580389a0122ed68691a6c0bfdbbce034
othmane errouissat <rouissatothman@gmail.com>
2024-06-04 16:15:37 +0100

    add the user api service file

---
+++ src/services/userService.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+
+export function registerClient(body) {
+    return api.post('/client/register', body)
+}
+export function registerUser(body) {
+    return api.post('/client/register/user', body)
+}
+export function verifyEmail(id,token) {
+    return api.get(`/client/verify/${id}/${token}`)
+}
+export function forgetPassword (email) {
+    return api.post('/client/forgot-password',{email : email})
+}
+export function resetPassword (token, body) {
+    return api.post(`/client/reset-password/${token}`, body)
+}
+
+export function loginUserApi(email,password,endpoint) {
+    console.log(endpoint)
+    return api.post(`/${endpoint}/login`, {email : email, password : password})
+}
