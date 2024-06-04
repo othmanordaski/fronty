658e45fc438382cc803e7302b86ff855e28e1e59
othmane errouissat <rouissatothman@gmail.com>
2024-06-04 16:23:13 +0100

    refactor the code and add a dynamic image avatar

--- src/pages/Dashboards/admin/avatar-nav.jsx
+++ src/pages/Dashboards/admin/avatar-nav.jsx
@@ -10,14 +10,17 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
   } from "@/components/ui/dropdown-menu"
-  
+  import { useAuth } from "@/contexts/AuthContext"
+
   export function AdminAvatar() {
+    const { user, logout } = useAuth();
+    
     return (
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="ml-2 relative h-8 w-8 rounded-full">
             <Avatar className="h-8 w-8">
-              <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+              <AvatarImage src={user.image} alt="@shadcn" />
               <AvatarFallback>SC</AvatarFallback>
             </Avatar>
           </Button>
@@ -25,9 +28,9 @@ import {
         <DropdownMenuContent className="w-56" align="end" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">shadcn</p>
+              <p className="text-sm font-medium leading-none">{user?.username}</p>
               <p className="text-xs leading-none text-muted-foreground">
-                m@example.com
+                {user?.email}
               </p>
             </div>
           </DropdownMenuLabel>
@@ -41,7 +44,7 @@ import {
             </DropdownMenuItem>
           </DropdownMenuGroup>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>
+          <DropdownMenuItem onClick={logout} >
             Log out
           </DropdownMenuItem>
         </DropdownMenuContent>
